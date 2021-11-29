df96154932c716884dc29c7a3f14debcd2471258
Kynel <47177212+Kynel@users.noreply.github.com>
2021-11-29 17:59:40 +0900

    tsc-watch, 디렉토리 분류를 통한 개발환경 구축

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'Jun', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log();
+};
+//# sourceMappingURL=index.js.map
